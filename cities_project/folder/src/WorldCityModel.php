693abc15f9921dd340d2c5a1db16f3beaff2a3fc
Ddiiaanaa <114647803+Ddiiaanaa@users.noreply.github.com>
2025-12-10 15:36:12 +0200

    Add files via upload

---
+++ cities_project/folder/src/WorldCityModel.php
@@ -0,0 +1,20 @@
+<?php
+
+class WorldCityModel {
+    public function __construct(
+        public int $id,
+        public string $city,
+        public string $country,
+        public string $iso2,
+        public string $adminName,
+        public int $population
+    ) {}
+
+    public function getCityWithCountry(): string {
+        return "{$this->city} ({$this->getFlag()} $this->country)";
+    }
+
+    public function getFlag(): string {
+        return get_flag_for_country($this->iso2);
+    }
+}
